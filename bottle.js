682b38c48a66a99bae4f26831ea0710debb9d8d4
ISHANCODERJR <104986815+ISHANCODERJR@users.noreply.github.com>
2024-03-23 16:50:39 +0530

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,35 @@
+bottle_img = ""
+status = ""
+
+function preload() {
+    img = loadImage('AC.jpg')
+}
+
+function createCanvas() {
+    canvas = createCanvas(640 , 420)
+    canvas.center()
+    objectDector = ml5.objectDector('cocossd' , modelLoaded)
+    document.getElementById("status").innerHTML = "status : detecting object"
+}
+
+function draw() {
+    image(img , 0 , 0 , 640 , 420)
+    fill("#FF0000")
+    text("dog" , 45 , 75)
+    noFill()
+    stroke("#FF0000")
+    rect(30 , 90 , 450 , 350)
+}
+
+function modelLoaded() {
+    console.log("model is loaded");
+    status = true
+    objectDector.decting(img , gotResult)
+}
+function gotResult() {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+    }
+}
